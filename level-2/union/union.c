03f37c53ce3a527635b13139d282e5053050ed4f
AslanAmca <aliaslan_61@outlook.com>
2023-01-18 23:52:12 +0300

    level-2 eklendi

--- level-2/union/union.c
+++ level-2/union/union.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_strcat(char *dest, char *src)
+void ft_strcat(char *dest, char *src)
 {
 	int dest_len = 0;
 	while (dest[dest_len] != '\0')
@@ -15,13 +15,38 @@ int ft_strcat(char *dest, char *src)
 		src_index++;
 	}
 	dest[dest_len] = '\0';
-	return dest_len;
+}
+
+int ft_strchr_index(char *string, int index, char character)
+{
+	int i = 0;
+
+	while (i < index)
+	{
+		if (string[i] == character)
+			return 1;
+		i++;
+	}
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-		int char_count = ft_strcat(argv[1], argv[2]);
+		// stringleri birleştirip tek bir string haline getirmeliyiz.
+		ft_strcat(argv[1], argv[2]);
+
+		int i = 0;
+		while (argv[1][i] != '\0')
+		{
+			// Mevcut karakter, kendisinden önce gelen karakterler arasında yoksa yaz.
+			if (ft_strchr_index(argv[1], i, argv[1][i]) == 0)
+				write(1, &argv[1][i], 1);
+			i++;
+		}
+
 	}
+	write(1, "\n", 1);
+	return 0;
 }
